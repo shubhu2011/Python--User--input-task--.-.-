964d5a6ceff7dfcd1e44963ee5135907c799af74
shubhu2011 <shubhita652@gmail.com>
2025-10-04 16:15:32 +0530

    Add files via upload

---
+++ Number Comparer.py
@@ -0,0 +1,8 @@
+numb1=int(input("Enter Your number"))
+numb2=int(input("Enter Your number"))
+if numb1>numb2:
+    print(numb1 ,"is greater than", numb2)
+if numb1<numb2:
+    print(numb1 ,"is less than", numb2)
+if numb1==numb2:
+    print(numb1 ,"is equal to", numb2)
